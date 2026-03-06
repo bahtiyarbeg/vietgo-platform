0a80ec3358600b49aa605ecf6864a720203d0c90
bahtiyarbeg <bahtiyarbeg@gmail.com>
2026-03-06 17:59:24 +0700

    Update BookingController.php

--- backend/app/Controllers/BookingController.php
+++ backend/app/Controllers/BookingController.php
@@ -2,10 +2,20 @@
 
 class BookingController {
 
-function createBooking(){
+    public function createBooking(){
 
-echo "booking created";
+        $data = json_decode(file_get_contents("php://input"), true);
 
-}
+        $name = $data["name"];
+        $hotel = $data["hotel"];
+        $tour = $data["tour"];
+        $date = $data["date"];
+
+        echo json_encode([
+            "status" => "success",
+            "message" => "Booking created",
+            "tour" => $tour
+        ]);
+    }
 
 }
